d5ace17679e96a0ea067c170516b060bd8853a7a
Isaac Samsky <github+ibsamsky@cark.moe>
2026-07-27 23:53:08 -0400

    argon: (some) torrent config

--- systems/argon/services/qbittorrent.nix
+++ systems/argon/services/qbittorrent.nix
@@ -4,13 +4,31 @@
   services.qbittorrent = {
     enable = true;
 
-    extraArgs = [ "--confirm-legal-notice" ];
+    openFirewall = true;
 
     serverConfig = {
+      BitTorrent = {
+        Session = {
+          AddTrackersFromURLEnabled = true;
+          AnonymousModeEnabled = true;
+          AdditionalTrackersURL = "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt";
+          GlobalMaxRatio = 2;
+          GlobalMaxSeedingMinutes = 4320; # 72 hours
+          QueueingSystemEnabled = false;
+          ShareLimitAction = "Remove";
+        };
+      };
+
+      LegalNotice.Accepted = true;
+
       Preferences = {
+        Advanced.markOfTheWeb = false;
         WebUI = {
           AlternativeUIEnabled = true;
           RootFolder = "${pkgs.vuetorrent}/share/vuetorrent";
+
+          Username = "admin";
+          Password_PBKDF2 = "r5K+xdm0azWaEpnsIzeIYw==:WL/949LD7ZlAzzVvLVK7KMVylm3J6MKllLAzYgOXQ3jAERmPeayH/TqId2G180DWjJpGBZhZVZimEZQ+HZiEGg==";
         };
       };
     };
