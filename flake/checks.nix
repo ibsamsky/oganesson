f1f57a989e07fe40639e40289c7444ec2c412d0e
Isaac Samsky <github+ibsamsky@cark.moe>
2026-09-07 00:41:52 -0400

    feat: build systems in flake checks

--- flake/checks.nix
+++ flake/checks.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, self, ... }:
 
 let
   # looks confusing because point-free style
@@ -6,11 +6,29 @@ let
   flattenOneLevel = lib.concatMapAttrs (
     outerName: lib.mapAttrs' (innerName: lib.nameValuePair "${outerName}-${innerName}")
   );
+
+  # map a set of NixOS configurations to a set of derivations that build the system's toplevel configuration
+  cfgsToDrvs = lib.mapAttrs (_: c: c.config.system.build.toplevel);
+
+  # filter configurations that are compatible with a given system
+  filterCompatibleCfgs = system: lib.filterAttrs (_: c: c.pkgs.stdenv.hostPlatform.system == system);
 in
 {
-  perSystem = { self', ... }: {
-    checks = flattenOneLevel {
-      inherit (self') devShells;
+  perSystem =
+    { self', system, ... }:
+    let
+      # map each set of configurations to a set of derivations
+      # flattened to e.g. `nixosConfigurations-<name>`
+      configs = lib.mapAttrs (_: cs: cfgsToDrvs (filterCompatibleCfgs system cs)) {
+        inherit (self) nixosConfigurations;
+      };
+    in
+    {
+      checks = flattenOneLevel (
+        configs
+        // {
+          inherit (self') devShells;
+        }
+      );
     };
-  };
 }
